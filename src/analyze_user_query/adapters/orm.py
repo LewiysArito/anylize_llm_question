e823df737f91c55d537caec70e8d4dd158d8293c
LewiysArito <nikita.kasimov.1044@gmail.com>
2025-10-28 02:14:27 +0300

    feat: add domain model and ORM mapping for user query analysis

---
+++ src/analyze_user_query/adapters/orm.py
@@ -0,0 +1,27 @@
+from typing import Tuple
+from analyze_user_query.domain import model
+from src.analyze_user_query.clickhouse_helper import (
+    Array, Column, Date, EngineType, FixedString, Function, IPv4, Mapper, String, Table, UUID  
+)
+
+analize_user_llm_query = Table(
+    "analize_user_llm_query",
+    EngineType.MERGETREE, 
+    ["date", "country_code", "language_code", "model_llm"],
+    Function("toYYYYMM(date)"),
+    None,
+    Column("event_id", UUID(), False),
+    Column("text", String(), False),
+    Column("date", Date(), False),
+    Column("themes", Array(FixedString(128)), False),
+    Column("language_code", FixedString(3), False),
+    Column("country_code", FixedString(3), True, "NULL"),
+    Column("user_ip", IPv4(), True, "NULL"),
+    Column("model_llm", String(), False)
+)
+
+def start_mappers() -> Tuple[Mapper, ...]:
+    analize_user_llm_query_mapper = Mapper(model.AnalyzedUserQuery, analize_user_llm_query)
+    return (analize_user_llm_query_mapper,)
+
+analize_user_llm_query_mapper,  = start_mappers()
--- src/analyze_user_query/domain/model.py
+++ src/analyze_user_query/domain/model.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from datetime import datetime
+from typing import List, Optional
 from uuid import UUID
 from ipaddress import IPv4Address
 
@@ -13,3 +14,14 @@ class DataUserQuery(unsafe_hash=True):
     ip_address: IPv4Address
     raw_text: str
     timestamp: datetime
+
+@dataclass(frozen=True)
+class AnalyzedUserQuery(unsafe_hash=True):
+    event_id: UUID
+    text: str
+    date: datetime
+    themes: List[str]
+    language_code: str
+    country_code: Optional[str]
+    user_ip: IPv4Address
+    model_llm: str
