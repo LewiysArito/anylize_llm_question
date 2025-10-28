dd8e4f6f8cd534470f84561a876b2b83416fe7bd
LewiysArito <nikita.kasimov.1044@gmail.com>
2025-10-29 01:51:13 +0300

    fix: change Column type to Table in create_table argument

--- src/analyze_user_query/adapters/init_db.py
+++ src/analyze_user_query/adapters/init_db.py
@@ -1,14 +1,14 @@
 import asyncio
 from clickhouse_connect.driver.asyncclient import AsyncClient
-from analyze_user_query.clickhouse_helper import Column 
+from analyze_user_query.clickhouse_helper import Table
 from analyze_user_query.adapters.orm import analize_user_llm_query
 from analyze_user_query.adapters.repository import get_async_client
 
-async def create_table(client: AsyncClient, table: Column) -> None:
+async def create_table(client: AsyncClient, table: Table) -> None:
     sql = table.generate_sql_for_create(if_not_exists=True)
     await client.command(sql)
 
-async def init_tables_clickhouse_if_not_exists(client: AsyncClient, tables: list[Column]) -> None:
+async def init_tables_clickhouse_if_not_exists(client: AsyncClient, tables: list[Table]) -> None:
     tasks = [create_table(client, table) for table in tables]
     await asyncio.gather(*tasks)
 
